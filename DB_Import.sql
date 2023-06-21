543b8350f893f45a70425daceaabee1fe1f5aca4
Richard <richardprax2003@gmail.com>
2023-06-21 14:57:47 +0200

    created import skript, nothing added (Bday)

---
+++ DB_Import.sql
@@ -0,0 +1,11 @@
+-- 
+-- Datenbank: EcoWheels 
+-- erstellt am 
+-- durch Projektgruppe C5
+-- Datenbank mit Tabellen für EcoWheels Verwaltungssystem
+-- Importskript für Testdaten
+
+-- --------------------------------------------------------
+
+USE EW_DB;
+
